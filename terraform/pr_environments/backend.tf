bdd26875aee441e90b65b8192c0de1c1a1221ff8
Jakub Popiolek <grimurin@gmail.com>
2025-04-28 23:33:10 +0200

    Comit

--- terraform/pr_environments/backend.tf
+++ terraform/pr_environments/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "pr-gamegather-rg"
-    storage_account_name = "previewenvironment"
-    container_name       = "dev-tfstate"
-    key                 = "dev.ui.terraform.tfstate"
+    resource_group_name  = ""
+    storage_account_name = ""
+    container_name       = ""
+    key                 = ""
   }
 }
